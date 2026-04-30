b072c15a06f3a98ca735329e1feb6777500bc4fd
Ankit Thakur <111188389+Ankitthakur2025@users.noreply.github.com>
2026-04-30 15:49:20 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,7 +83,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Data Analyst ", " Python developer", "SQL developer", "Aspiring Data Scientist"],
+    strings: ["Data Analyst ", " Power BI developer", "SQL developer", "Aspiring Data Scientist"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
